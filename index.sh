0eaaad0fc87e31760641f6c94a0804a5cbd42bd3
冯世雨 <fengshiyu02@meituan.com>
2018-09-19 19:17:40 +0800

    update: 提交

--- index.sh
+++ index.sh
@@ -1,6 +1,10 @@
 #!/usr/bin/env bash
-set -u # 增加报错
-set -x # 提示输出内容是什么
 
-echo "hello"
+set -euo pipefail # 合并写法
+# set -u # 增加报错
+# # set -x # 提示输出内容是什么
+# set -e # 只要脚本发生错误，立刻终止继续运行
+# set -o pipefail # 防止管道报错仍继续执行
 
+# foo || { echo "未知命令或命令有误"; exit 1; }
+echo 1
